c7c7a06f50d747ac113cf1510049eb92d3252d13
Erik Panek <ebpk6thfn@users.noreply.github.com>
2026-04-19 16:58:50 +0800

    change verbose default to false (was left on for debugging)

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	)
 
 	flag.IntVar(&port, "port", getEnvInt("PORT", defaultPort), "HTTP server port")
-	flag.BoolVar(&verbose, "verbose", true, "Enable verbose logging") // default to true for my local debugging
+	flag.BoolVar(&verbose, "verbose", false, "Enable verbose logging")
 	flag.Parse()
 
 	if verbose {
